df45c6a7d0b4859bb5661a4d7903c621006217db
aristov <vv.aristov@gmail.com>
2014-07-02 00:35:06 +0400

    dont use client js

--- blocks/server/server.node.js
+++ blocks/server/server.node.js
@@ -33,7 +33,6 @@ app.get('/', function(req, res) {
         block: 'page',
         title: 'Twitjeet!',
         styles: [{ elem: 'css', url: '_index.css' }],
-        scripts: [{ elem: 'js', url: '_index.js' }],
         mods: { theme: 'normal' },
         content: [
             {
